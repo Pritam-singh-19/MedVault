652860f8df4a5412f813553b880e686608a50661
sukuna-19 <pritamsing1906@gmail.com>
2025-09-29 23:32:09 +0530

    Updated frontend UI with new changes

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyXXXXX-XXXXX",
-  authDomain: "medvault.firebaseapp.com",
-  projectId: "medvault",
-  storageBucket: "medvault.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef123456",
-  measurementId: "G-XXXXXXX"
+  apiKey: "AIzaSyBeZ2ZQsoMKC_B_y-m8mYe5jiMNCnuRYwo",
+  authDomain: "medvault-4d54b.firebaseapp.com",
+  projectId: "medvault-4d54b",
+  storageBucket: "medvault-4d54b.appspot.com",
+  messagingSenderId: "599183176841",
+  appId: "1:599183176841:web:6b8035e4feaad2685ac258",
+  measurementId: "G-W39313KTGF"
 });
 
 const messaging = firebase.messaging();
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,17 +1,17 @@
 import { initializeApp } from 'firebase/app';
-import { getMessaging } from 'firebase/messaging';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyXXXXX-XXXXX",
-  authDomain: "medvault.firebaseapp.com",
-  projectId: "medvault",
-  storageBucket: "medvault.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef123456",
-  measurementId: "G-XXXXXXX"
+  apiKey: "AIzaSyBeZ2ZQsoMKC_B_y-m8mYe5jiMNCnuRYwo",
+  authDomain: "medvault-4d54b.firebaseapp.com",
+  projectId: "medvault-4d54b",
+  storageBucket: "medvault-4d54b.appspot.com",
+  messagingSenderId: "599183176841",
+  appId: "1:599183176841:web:6b8035e4feaad2685ac258",
+  measurementId: "G-W39313KTGF"
 };
 
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
-export { messaging };
+export { messaging, getToken, onMessage };
